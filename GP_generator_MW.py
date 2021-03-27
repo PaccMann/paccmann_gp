f04ee8bcdad3d1c99607c0e0123fc1825be5d624
astridstroobants <81263413+astridstroobants@users.noreply.github.com>
2021-03-27 13:29:32 +0100

    debugging

--- GP_generator_MW.py
+++ GP_generator_MW.py
@@ -5,7 +5,7 @@ from rdkit import Chem
 from rdkit.Chem.Descriptors import MolWt
 from skopt import gp_minimize
 
-class GP_generator_for_MW(nnModule):
+class GP_generator_for_MW():
     """ SVAE GP generator minimisation functions """
 
     def __init__(self, SVAEmodel):
@@ -28,7 +28,7 @@ class GP_generator_for_MW(nnModule):
             tomsiles: boolean to specify if output should be smiles (True) or   numerical sequence (False)
         """
 
-        mols_numerical=self.SVAEmodel.generate(latent_point,     prime_input=torch.LongTensor([self.SVAEmodel.smiles_language.start_index]), end_token=torch.LongTensor([self.SVAEmode.smiles_language.stop_index]))
+        mols_numerical=self.SVAEmodel.generate(latent_point,     prime_input=torch.LongTensor([self.SVAEmodel.smiles_language.start_index]), end_token=torch.LongTensor([self.SVAEmodel.smiles_language.stop_index]))
 
         # if smiles are required instead of selfies
         if tosmiles==True:
@@ -36,25 +36,44 @@ class GP_generator_for_MW(nnModule):
             self.SVAEmodel.smiles_language.token_indexes_to_smiles(num_mol.tolist()),
                 torch.cat([num_mol.long(),
                 torch.tensor(2 * [self.SVAEmodel.smiles_language.stop_index])]
-                )) for num_mol in iter(numeric_mol)]
+                )) for num_mol in iter(mols_numerical)]
 
             numericals = [sm[1] for sm in smiles_num_tuple]
 
             smiles = [self.SVAEmodel.smiles_language.selfies_to_smiles(sm[0])
                     for sm in smiles_num_tuple]
-            imgs = [Chem.MolFromSmiles(s, sanitize=False) for s in smiles]
+            imgs = [Chem.MolFromSmiles(s, sanitize=True) for s in smiles]
 
             valid_idxs = [ind for ind in range(len(imgs)) if imgs[ind] is not None]
 
             smiles = [smiles[ind] for ind in range(len(imgs))
                     if not ( imgs[ind] is None)]
 
-            return smiles
+            return self.generate_smiles(latent_point) if smiles==[] else smiles
         else:
             return mols_numerical
 
 
-    def MW_minimisation_function(self, latentlist, targetMW):
+    def Optimisation_process(self,targetMW):
+        """
+        Optimisation with GP
+
+        Args:
+            targetMW: target molecular weight of type float or int
+        """
+        self.target=targetMW
+        res = gp_minimize(self.MW_minimisation_function,
+                      [(-5.0, 5.0)]*256, #256 is latent dimensions
+                      acq_func="EI",
+                      n_calls=100,
+                      n_initial_points=20,
+                      initial_point_generator='random',
+                      random_state=1234)
+
+        return res
+
+
+    def MW_minimisation_function(self, latentlist):
         """
         Function to minimise in Bayesian optimisation
 
@@ -69,23 +88,4 @@ class GP_generator_for_MW(nnModule):
             smiles=self.generate_smiles(latent_point)
         mweights=[MolWt(Chem.MolFromSmiles(smile)) for smile in smiles]
 
-        return abs(targetMW-mweights[0])
-
-
-    def Optimisation_process(self,targetMW):
-        """
-        Optimisation with GP
-
-        Args:
-            targetMW: target molecular weight of type float or int
-        """
-
-        res = gp_minimize(self.MW_minimisation_function(target=targetMW),
-                      [(-5.0, 5.0)]*256, #256 is latent dimensions
-                      acq_func="EI",
-                      n_calls=100,
-                      n_initial_points=20,
-                      initial_point_generator='random'
-                      random_state=1234)
-
-        return res
+        return abs(self.target-mweights[0])
